a2f8279606927a9e6f80f9f9d0b36b96485bc6ef
Shakira <shakira.r.salazar@gmail.com>
2021-08-20 16:54:38 +0100

    maybe did too much

--- spec/fizzbuzz_spec.rb
+++ spec/fizzbuzz_spec.rb
@@ -1,7 +1,20 @@
 require_relative '../lib/fizzbuzz'
 
 describe 'fizzbuzz' do
+  it 'input is type of integer'
+    fizzbuzz.should_recieve(Integer)
+		fizzbuzz("hello")
+  end
 	it 'returns "fizz" when passed 3' do
     expect(fizzbuzz(3)).to eq 'fizz'
+  end
+	it 'returns "buzz" when passed 5' do
+    expect(fizzbuzz(5)).to eq 'buzz'
+  end
+	it 'returns "fizzbuzz" when divisble by 3 and 5' do
+    expect(fizzbuzz(15)).to eq 'fizzbuzz'
+  end
+  it 'returns number when not divisble by 3 or 5' do
+    expect(fizzbuzz(7)).to eq 7
   end
 end
